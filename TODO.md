f1ef2087eb7e70ce685586ac08ed819a40af9b87
Absobel <imkonnu@gmail.com>
2024-01-25 11:02:48 +0100

    nvmd quadtree fonctionne que dalle

--- TODO.md
+++ TODO.md
@@ -1,15 +1,10 @@
 # Obligatoire
 
+[ ] - Réparer quadtree
 [ ] - vitesse de la palette crée l'impulsion au lieu de la position (penser à garder la norme de la vitesse)
-[x] - faire en sorte que la balle part de la palette
-[x] - BUGFIX quadtree : correctement snap brick sur grid 
 [ ] - ajouter des vies
 [ ] - faire score (ajouter nb brique qqpart et donner ça à fct score)
 [ ] - ajouter la victoire
-[x] - utilisation de quadtree au lieu de pos de briques
-  [x] - quadtree
-  [x] - remplacer brique list par brique qtree
-  [x] - faire marcher ça avec plsu qu'une brique
 [ ] - faire une vraie acceleration au cours du temps
 [ ] - better ball collision
 [ ] - modulariser
@@ -20,6 +15,5 @@
 # Extensions
 
 [ ] - ajouter des niveaux
-[ ] - refaire la fonction qui crée les briques pour pouvoir faire des rects de briques
 [ ] - couleur de brique = score
 [ ] - je verrai plus tard
--- lib/init_values.ml
+++ lib/init_values.ml
@@ -24,11 +24,19 @@ module BriquesInit = struct
   let br_height = 50.
   let br_width = 100.
 
+  (* liste de briques de type : ((float*float) * Graphics.color) list *)
   (* Les briques peuvent être placées que sur une grille de briques, les coords vont snap sur la coord de grille la plus proche *)
   let br_list =
-    [ (250., 250.), Graphics.rgb (Random.int 255) (Random.int 255) (Random.int 255)
-    ; (300., 300.), Graphics.rgb (Random.int 255) (Random.int 255) (Random.int 255)
-    ]
+    let create_brick x y  =
+      (x, y), Graphics.rgb (Random.int 255) (Random.int 255) (Random.int 255) in
+    let rec fill acc x y max_x max_y=
+      if x > max_x then
+        fill acc 0. (y +. br_height) max_x max_y
+      else if y > max_y then
+        acc
+      else
+        fill ((create_brick x y) :: acc) (x +. br_width) y max_x max_y in
+    fill [] 0. (br_height*.4.) BoxInit.width BoxInit.height
 end
 
 module PaletteInit = struct
